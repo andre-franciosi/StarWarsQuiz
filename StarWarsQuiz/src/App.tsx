41d90434b4d194cd7d4058435bf017165ce7afec
André Franciosi <andref2210@hotmail.com>
2025-06-26 13:02:54 -0300

    first commit
    styling the welcome screen

---
+++ StarWarsQuiz/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the Star Wars Quiz</h1>
+      <h2>Press the button to start!</h2>
+      <button className="start-button">CLICK ME</button>
+    </>
+  );
+}
+
+export default App;
